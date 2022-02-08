80264ad13c630871a0beec9c1f734fd1b6afaca4
Calawente <72078382+Calawente@users.noreply.github.com>
2022-02-08 13:24:14 +0300

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,7 +2,16 @@ HEIGHT = 175
 WEIGHT = 90
 steps = int(input("Количество шагов: "))
 time = int(input("Время: "))
-def CalCalc(h,w,s,t):
+def cal_calc(h,w,s,t):
 	return 0.035 * w + ((s/t)**2 / h) * 0.029 * w
 
-print(f"Калорий сожжено: {CalCalc(HEIGHT,WEIGHT,steps,time)}; Пройденная дистанция: {steps/time}")
+print(f"Калорий сожжено: {cal_calc(HEIGHT,WEIGHT,steps,time)}; Пройденная дистанция: {steps/time}")
+l_step = HEIGHT / 4 + 0.37
+dist = steps * l_step / 100000
+print(f"Дистанция в км: {dist}")
+if dist < 2:
+	print("Пожнажми!")
+elif dist < 4:
+	print("Хорошо!")
+else:
+	print("Отлично!")
